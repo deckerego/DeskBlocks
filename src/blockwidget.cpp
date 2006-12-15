e173e7c5ee66fac36cadd3d1204ae4edc6484aed
jellis <jellis@9d53d0de-361a-0410-b5d5-aca9532bb6ba>
2006-12-15 23:21:26 +0000

    Breaking properties apart into two classes

---
+++ src/blockwidget.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by John T. Ellis   *
+ *   jtellis@alumni.indiana.edu   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QtGui>
+#include <math.h>
+
+#include "blockwidget.h"
+
+BlockWidget::BlockWidget()
+  : QWidget(0, Qt::FramelessWindowHint | Qt::X11BypassWindowManagerHint)
+{
+  bitmask = QBitmap(":/blocks/square.png");
+}
+
+QSize BlockWidget::sizeHint() const 
+{
+  //a^2 + b^2 = c^2. I remembered something from elementary school!
+  //In all fairness, this is actually square(side * (side * 2))
+  int boundingLength = (int)sqrt(LENGTH * (LENGTH << 1));
+  if(DEBUG) qDebug("Calculating size hint: %i", boundingLength);
+  return QSize(boundingLength, boundingLength);
+}
+
+void BlockWidget::setPosition(dReal *position) {
+  //Update position
+  int xPos = ABSOLUTE(position[0]);
+  int yPos = ABSOLUTE(position[1]);
+  
+  if(DEBUG) qDebug("Position: %i (%f), %i (%f)", xPos, position[0], yPos, position[1]);
+  
+  // Update Qt's position
+  move(xPos, yPos);
+}
+
+void BlockWidget::setPosition(QPoint position) {
+  dReal xPos = (dReal)position.x();
+  dReal yPos = (dReal)position.y();
+  
+  // Update ODE's position
+  dGeomSetPosition(geometry, RELATIVE(xPos), RELATIVE(yPos), 0);
+  if(DEBUG) qDebug("Moved to %f, %f", xPos, yPos);
+
+  // Update Qt's position
+  move(position);
+}
+
+void BlockWidget::resetRotation()
+{
+  dMatrix3 odeRotation;
+  dRSetIdentity (odeRotation);
+  dBodySetRotation(body, odeRotation);
+}
+
+void BlockWidget::setRotation(const dMatrix3 odeRotation)
+{
+  //ODE has its origin starting at the lower left-hand corner, while Qt's starts in the upper
+  //right. Since a positive angle represents a counter-clockwise turn in ODE, we'll need to invert
+  //the turn in Qt to make it clockwise (relative to Qt's coordinate system). The nice thing
+  //is that since this is a matrix identity ([a b][-b a]) we just have to swap values.
+  rotation = QMatrix(odeRotation[0], odeRotation[4], odeRotation[1], odeRotation[5], 0.0, 0.0);
+}
+
+void BlockWidget::paintEvent(QPaintEvent *)
+{
+  QLinearGradient linearGradient(0, 0, LENGTH, LENGTH);
+  linearGradient.setColorAt(0.0, Qt::white);
+  linearGradient.setColorAt(0.2, Qt::green);
+  linearGradient.setColorAt(1.0, Qt::black);
+  
+  QRegion maskedRegion(bitmask.transformed(rotation));
+  QPainterPath maskedPath;
+  maskedPath.addRegion(maskedRegion);
+  setMask(maskedRegion);
+  
+  QPainter painter(this);
+  painter.save();
+  painter.fillPath(maskedPath, linearGradient);
+  painter.restore();
+}
---
+++ src/blockwidget.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by John T. Ellis   *
+ *   jtellis@alumni.indiana.edu   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef QBLOCKWIDGETBLOCK_H
+#define QBLOCKWIDGETBLOCK_H
+
+#include <QWidget>
+#include <QPixmap>
+#include <QBitmap>
+#include <QMatrix>
+#include <ode/ode.h>
+
+#include "conventions.h"
+
+class DeskBlocks;
+
+/**
+  @author John T. Ellis <jtellis@alumni.indiana.edu>
+ */
+class BlockWidget : public QWidget
+{
+  Q_OBJECT
+      
+  public:
+    BlockWidget();
+    
+    QSize sizeHint() const;
+    
+    void resetRotation();
+    void setRotation(const dMatrix3 rotation);
+    
+    void setPosition(dReal *position);
+    void setPosition(const QPoint position);
+    
+  protected:
+    QMatrix rotation;
+    dBodyID body;
+    dGeomID geometry;
+    
+    void paintEvent(QPaintEvent *event);
+    
+  private:
+    QBitmap bitmask;
+};
+
+#endif
