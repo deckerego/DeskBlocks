0a122a77fad2615254471c30c65238b9e6aa4c06
jellis <jellis@9d53d0de-361a-0410-b5d5-aca9532bb6ba>
2007-01-15 06:36:49 +0000

    Added circle block

---
+++ src/circle.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *   Copyright (C) 2006 by John T. Ellis   *
+ *   jtellis@alumni.indiana.edu   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include <QtGui>
+
+#include "circle.h"
+
+Circle::Circle(DeskBlocks *parent, QPoint position)
+  : Block(parent, position, new QBitmap(":/blocks/circle.bmp"), LENGTH, LENGTH)
+{
+  dMass mass;
+  
+  dReal length = RELATIVE(LENGTH);
+  dReal density = RELATIVE(DENSITY);
+
+  //Define initial mass
+  dMassSetSphere(&mass, density, length / 2);
+  dBodySetMass(body, &mass);
+  
+  //Set collision space
+  geometry = dCreateSphere(parent->space, length / 2);
+  dGeomSetBody(geometry, body);
+}
